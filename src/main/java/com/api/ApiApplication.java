23b52b146545d46b57d940a7af9ae5b0973bee88
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-06-07 15:25:11 -0500

    Created Player class and added return List of Player in Main

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -1,9 +1,14 @@
 package com.api;
 
+import com.api.player.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,8 +20,16 @@ public class ApiApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World in JAVA!!";
+	public List<Player> hello() {
+		return List.of(
+				new Player(
+						1L,
+						"Amber",
+						"test22@gmail.com",
+						LocalDate.of(1990, Month.APRIL, 5),
+						32
+				)
+		);
 	}
 
 }
